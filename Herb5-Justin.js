b79ed3122d4aaa24e0bf83e1e261f25f8f24a1a6
Justin Lee <810636@seq.org>
2023-12-11 09:31:37 -0800

    1112 flock
    added some initial flocking code from lab

--- Herb5-Justin.js
+++ Herb5-Justin.js
@@ -13,4 +13,53 @@ class Herb5 extends Creature{
         this.wWidth = wrld.dims.width;
         this.wHeight = wrld.dims.height;
     }
+    Herb5.prototype.align = function (vehicles) {
+        let steer = new JSVector(0,0);
+        let sum=new JSVector(0,0);
+        let count=0;
+        for(let i=0;i<vehicles.length;i++){
+          if(this.loc.distance(vehicles[i].loc)<this.desiredAli&&vehicles[i]!==this){
+            sum.add(vehicles[i].vel);
+            count++;
+          }
+      
+        }
+        if(count>0){
+          sum.divide(vehicles.length);
+          sum.setMagnitude(this.maxSpeed);
+          steer=JSVector.subGetNew(sum,this.vel);
+          steer.limit(this.maxForce);
+        }
+        return steer;
+      }
+      
+      Herb5.prototype.cohesion = function (vehicles) {
+        let coh = new JSVector(0,0);
+        let sum=new JSVector(0,0);
+        let count=0;
+        let d=this.loc.distance(vehicles[i].loc);
+        for(let i=0;i<vehicles.length;i++){
+          if(d<this.desiredCoh&&d>0){
+            sum.add(vehicles[i].loc);
+            count++;
+          }
+      
+        }
+        if(count>0){
+          sum.divide(count);
+          return this.seek(sum);
+        } else {
+          return coh;
+        }
+      }
+      
+      Herb5.prototype.seek = function(target) {
+        // A vector pointing from the location to the target
+        let desired = JSVector.subGetNew(target,this.loc);  
+        desired.normalize();
+        desired.multiply(this.maxSpeed);
+        let steer = JSVector.subGetNew(desired,this.vel);
+        steer.limit(this.maxForce);
+        return steer;
+      }
 }
