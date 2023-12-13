4a36754ea0f97633cacadcbffa4c37402abf5996
Logan Thomas <103534716+812047@users.noreply.github.com>
2023-12-13 09:56:36 -0800

    h

--- world.js
+++ world.js
@@ -120,17 +120,7 @@ class World {
 
   loadEntities(numEntities, ctx, w, h) {
     //++++++++++++++++++++++++++++  load entities
-    for (let i = 0; i < 50; i++) {
-      let x = (Math.random() * this.dims.width)-this.dims.width/2;
-      let y = (Math.random() * this.dims.height) -this.dims.height/2;
-      let loc = new JSVector(x, y);
-      let dx = Math.random() * 2 - 1;
-      let dy = Math.random() * 2 - 1;
-      let vel = new JSVector(dx, dy);
-      let sz = Math.floor(Math.random()*5 + 3);
-      this.creatures.herb4.push(new Herb4LYT(loc, vel, sz, this));
-
-    }
+    
     for (let i = 0; i < 500; i++) {
       let x = (Math.random() * this.dims.width)-this.dims.width/2;
       let y = (Math.random() * this.dims.height) -this.dims.height/2;
@@ -146,12 +136,7 @@ class World {
 
   runCreatures() {
     let c = this.creatures;
-    for (let i = 0; i < c.herb4.length; i++) {
-      c.herb4[i].run();
-        if (c.herb4[i].isDead === true) {
-          c.herb4.splice(i, 1)
-        }
-    }
+    
     for (let i = 0; i < c.herb6.length; i++) {
       c.herb6[i].run(c.herb6);
         if (c.herb6[i].isDead === true) {
