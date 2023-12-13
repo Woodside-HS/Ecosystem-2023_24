193efcccb3e0cbb93f94e6e8aa9d8e6a6a7e0a49
Jogn <815751@seq.org>
2023-12-13 10:29:30 -0800

    Update pred4jdg.js

--- pred4jdg.js
+++ pred4jdg.js
@@ -9,6 +9,7 @@ class Pred4jdg extends Creature {
         this.clr = this.getRandomColor();
         this.size = sz;
         this.wrld = wrld;
+        this.target = new JSVector(0, 0);
         this.maxSpeed = 1;
         this.ctx = wrld.ctxMain;
         this.wWidth = wrld.dims.width;
@@ -45,6 +46,7 @@ class Pred4jdg extends Creature {
     //++++++++++++++++++++++++++++++++ creature methods
     run() {
         this.update();
+        
         this.checkEdges();
         this.render();
         this.flock(this.wrld.creatures.pred4);
@@ -61,9 +63,40 @@ class Pred4jdg extends Creature {
         this.loc.add(this.vel);
 
     }
+    seek(target) {
+        let desired = JSVector.subGetNew(target, this.loc);
+        desired.normalize();
+        desired.multiply(this.maxSpeed);
+        let steer = new JSVector.subGetNew(desired, this.vel);
+
+        steer.limit(this.maxSpeed);
+
+        this.applyForce(steer);
+    }
+
+    applyForce(force) {
+        this.acc.add(force);
+    }
+    arrive(target) {
+        let desired = JSVector.subGetNew(target,this.loc);
+     
+        let d = desired.getMagnitude();
+        desired.normalize();
+        if (d < 100) {
+          let m = map(d,0,100,0,maxspeed);
+          desired.mult(m);
+        } else {
+          desired.mult(maxspeed);
+        }
+     
+        let steer = JSVector.subGetNew(desired,this.vel);
+        steer.limit(maxforce);
+        applyForce(steer);
+      }
 
 
     flock(array) {
+        this.seek(this.target);
         //  flock force is the accumulation of all forces
         this.flockForce = new JSVector(0, 0);
         // set up force vectors to be added to acc
@@ -83,7 +116,7 @@ class Pred4jdg extends Creature {
         this.flockForce.add(ali);
         this.flockForce.add(coh);
         this.acc.add(this.flockForce);
-       // console.log("flocking!");
+        // console.log("flocking!");
     }
     //+++++++++++++++++++++++++++++++++  Flocking functions
     separate(array) {
@@ -107,7 +140,7 @@ class Pred4jdg extends Creature {
             dir = JSVector.subGetNew(escapeVector, this.vel);
             dir.limit(this.maxForce);
         }
-       //console.log("separating!");
+        //console.log("separating!");
         return dir;
     }
 
@@ -128,115 +161,127 @@ class Pred4jdg extends Creature {
         //console.log("aligning!");
         return sum;
     }
-  
-cohesion(array) {
-    let seekVector = new JSVector(0, 0);
-    let count = 0;
-    let dir = new JSVector();
-    for (let i = 0; i < array.length; i++) {
-        let distance = this.loc.distance(array[i].loc);
-        if (distance > 0 && distance < 50) {
-            let oppVector = JSVector.subGetNew(array[i].loc, this.loc);
-            oppVector.normalize();
-            oppVector.divide(distance);
-            seekVector.add(oppVector);
-            count++;
+
+    cohesion(array) {
+        let seekVector = new JSVector(0, 0);
+        let count = 0;
+        let dir = new JSVector();
+        for (let i = 0; i < array.length; i++) {
+            let distance = this.loc.distance(array[i].loc);
+            if (distance > 0 && distance < 50) {
+                let oppVector = JSVector.subGetNew(array[i].loc, this.loc);
+                oppVector.normalize();
+                oppVector.divide(distance);
+                seekVector.add(oppVector);
+                count++;
+            }
+        }
+        if (count > 0) {
+            seekVector.divide(count);
+            seekVector.normalize();
+            seekVector.multiply(this.maxSpeed);
+            dir = JSVector.subGetNew(seekVector, this.vel);
+            dir.limit(this.maxForce);
+        }
+        //console.log("cohesioning!");
+        return dir;
+    }
+
+    hunt(target) {
+        // A vector pointing from the location to the target
+        let desired = JSVector.subGetNew(target, this.loc);
+        let dir = JSVector.subGetNew(desired, this.vel);
+        return dir;
+    }
+    closestTarget() {
+        let distance = 0;
+        let closestCreature;
+        for (let i = 1; i < wrld.creatures.herb1.length; i++) {
+            const currentDistance = this.loc.distance(wrld.creatures.herb1.length[i]);
+            if (currentDistance < distance) {
+                distance = creatures[i];
+                distance = currentDistance;
+            }
+        }
+        return (distance, closestCreature);
+    }
+    attack(target) {
+
+    }
+    //+++++++++++++++++++++++++++++++++  Flocking functions
+    /* 
+     Vehicle.prototype.render = function () {
+         let ctx = game.ctx;
+         ctx.save();
+         ctx.translate(this.loc.x, this.loc.y);
+         ctx.rotate(this.vel.getDirection() + Math.PI / 2); //offset 90 degrees
+         ctx.beginPath();
+         ctx.strokeStyle = this.clr;
+         ctx.fillStyle = this.clr;
+         ctx.moveTo(0, -this.scl);
+         ctx.lineTo(-this.scl, this.scl);
+         ctx.lineTo(0, 0);
+         ctx.lineTo(this.scl, this.scl);
+         ctx.closePath();
+         ctx.stroke();
+         ctx.fill();
+         ctx.restore();
+     }
+     */
+    checkEdges() {
+        if (this.loc.x >= world.dims.width / 2 || this.loc.x <= -world.dims.width / 2) {
+            this.vel.x *= -1;
+        }
+        if (this.loc.y >= world.dims.height / 2 || this.loc.y < -world.dims.height / 2) {
+            this.vel.y *= -1;
         }
     }
-    if (count > 0) {
-        seekVector.divide(count);
-        seekVector.normalize();
-        seekVector.multiply(this.maxSpeed);
-        dir = JSVector.subGetNew(seekVector, this.vel);
-        dir.limit(this.maxForce);
-    }
-    //console.log("cohesioning!");
-    return dir;
-}
+    render() {
+        //  render balls in world
 
- hunt(target) {
-    // A vector pointing from the location to the target
-    let desired = JSVector.subGetNew(target, this.loc);
-    let dir = JSVector.subGetNew(desired, this.vel);
-    return dir;
-}
- closestTarget() {
-    let distance = 0;
-    let closestCreature;
-    for (let i = 1; i < wrld.creatures.herb1.length; i++) {
-        const currentDistance = this.loc.distance(wrld.creatures.herb1.length[i]);
-        if (currentDistance < distance) {
-            distance = creatures[i];
-            distance = currentDistance;
-        }
-    }
-    return (distance, closestCreature);
-}
- attack(target) {
+        let ctx = this.ctx;
+        ctx.save();
+        ctx.translate(this.loc.x, this.loc.y);
+        ctx.rotate(this.vel.getDirection() + Math.PI / 2);
+        ctx.beginPath();
 
-}
-//+++++++++++++++++++++++++++++++++  Flocking functions
-/* 
- Vehicle.prototype.render = function () {
-     let ctx = game.ctx;
-     ctx.save();
-     ctx.translate(this.loc.x, this.loc.y);
-     ctx.rotate(this.vel.getDirection() + Math.PI / 2); //offset 90 degrees
-     ctx.beginPath();
-     ctx.strokeStyle = this.clr;
-     ctx.fillStyle = this.clr;
-     ctx.moveTo(0, -this.scl);
-     ctx.lineTo(-this.scl, this.scl);
-     ctx.lineTo(0, 0);
-     ctx.lineTo(this.scl, this.scl);
-     ctx.closePath();
-     ctx.stroke();
-     ctx.fill();
-     ctx.restore();
- }
- */
-checkEdges() {
-    if (this.loc.x >= world.dims.width / 2 || this.loc.x <= -world.dims.width / 2) {
-        this.vel.x *= -1;
-    }
-    if (this.loc.y >= world.dims.height / 2 || this.loc.y < -world.dims.height / 2) {
-        this.vel.y *= -1;
-    }
-}
- render() {
-    //  render balls in world
-    let ctx = this.ctx;
-    ctx.beginPath();
-    ctx.fillStyle = this.clr;
-    ctx.arc(this.loc.x, this.loc.y, this.size, 0, 2 * Math.PI, false);
-    ctx.fill();
-    //  render balls in mini map
-}
+        ctx.moveTo(0, -15);
+        ctx.lineTo(-5, 5);
+        ctx.lineTo(0, 0);
+        ctx.lineTo(5, 5);
+        ctx.closePath();
+        ctx.stroke();
 
- getRandomColor() {
-    //  List of hex color values for movers
-    let colors = [
-        // "#7102AB",
-        "#ab0256",
-        "#0285ab",
-        "#02ab1a",
-        "#ab5302",
-        "#773e26",
-        "#ab0256",
-        "#257874",
-        "#78254e",
-        "#787725",
-        "#1102AB",
-        "#000256",
-        "#0200ab",
-        "#02ab00",
-        "#AAAA02",
-        "#77FFFF",
-        "#ab0006",
-        "#000874",
-        "#782000"
-    ];
-    let index = Math.floor(Math.random() * colors.length);
-    return colors[index];
+        ctx.fillStyle = this.clr;
+        ctx.fill();
+        ctx.restore();
+        //  render balls in mini map
+    }
+
+    getRandomColor() {
+        //  List of hex color values for movers
+        let colors = [
+            // "#7102AB",
+            "#ab0256",
+            "#0285ab",
+            "#02ab1a",
+            "#ab5302",
+            "#773e26",
+            "#ab0256",
+            "#257874",
+            "#78254e",
+            "#787725",
+            "#1102AB",
+            "#000256",
+            "#0200ab",
+            "#02ab00",
+            "#AAAA02",
+            "#77FFFF",
+            "#ab0006",
+            "#000874",
+            "#782000"
+        ];
+        let index = Math.floor(Math.random() * colors.length);
+        return colors[index];
+    }
 }
- }
