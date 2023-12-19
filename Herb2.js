648786a59c77767b12efdbce0d872405c7d89a8b
anorakthegreat <sanjanksen4@gmail.com>
2023-12-18 23:42:47 -0800

    more changes, radius bigger

--- Herb2.js
+++ Herb2.js
@@ -5,22 +5,21 @@ class Herb2 extends Creature{
         this.loc = loc
         this.vel = vel
         this.targetLoc = new JSVector(100, 100)
+        this.vec = new JSVector(200, 200)
+        this.time = 0;
     }
 
     run(){
         super.run()
     }
 
-    getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) ) + min;
-    }
+    
 
     update(){
+        this.time++
 
         let runAway = false
 
-        let num = getRndInteger(0, 1)
-        console.log(num)
 
         // let targetLoc = new JSVector(100, 100)
 
@@ -31,15 +30,6 @@ class Herb2 extends Creature{
             this.targetLoc = new JSVector(randX, randY)
         }
 
-        let dir = JSVector.subGetNew(this.targetLoc, this.loc)
-
-
-        // dir.multiply(0.1)
-        // let acceleration = dir;
-        // this.vel.add(acceleration)
-        // this.loc.add(this.vel)
-        // let dir = JSVector.subGetNew(targetLoc, this.loc)
-
         let ctx = this.ctx;
         ctx.strokeStyle = "rgba(255, 0, 0, 55)"
         ctx.fillStyle = "rgba(255, 0, 0, 55)"
@@ -48,6 +38,9 @@ class Herb2 extends Creature{
         ctx.stroke();
         ctx.fill() 
 
+
+        let dir = JSVector.subGetNew(this.targetLoc, this.loc)
+
         if (dir.getMagnitude() > 1) {
             dir.setMagnitude(1)
 
@@ -55,7 +48,35 @@ class Herb2 extends Creature{
         dir.multiply(0.5)
         let acceleration = dir;
 
+        let radius = 75
 
+        if(this.time % 75 == 0){
+            this.vec = new JSVector(Math.floor(Math.random() * (800 - 0 + 1) + 0), Math.floor(Math.random() * (800 - 0 + 1) + 0))
+
+        }
+
+        ctx.strokeStyle = "rgba(255, 0, 0, 55)"
+        ctx.fillStyle = "rgba(255, 0, 0, 55)"
+        ctx.beginPath();
+        ctx.arc(this.vec.x, this.vec.y, radius, Math.PI * 2, 0, false);
+        ctx.stroke();
+        ctx.fill() 
+
+        let newrandX = Math.floor(Math.random() * (800 - 0 + 1) + 0);
+        let newrandY = Math.floor(Math.random() * (600 - 0 + 1) + 0);
+        let newRandVec = new JSVector(newrandX, newrandY)
+
+        let dirNew = JSVector.subGetNew(newRandVec, this.loc)
+
+        if (dirNew.getMagnitude() > 1) {
+            dirNew.setMagnitude(1)
+
+        }
+        dirNew.multiply(1)
+
+        if(this.loc.distance(this.vec) < radius){
+            runAway = true
+        }
 
         if(!runAway){
             this.vel.add(acceleration)
@@ -63,8 +84,10 @@ class Herb2 extends Creature{
     
             this.loc.add(this.vel)
         } else {
-            let newVel = new JSVector(1, 1)
-            this.loc.add(newVel)
+            this.vel.add(dirNew)
+            this.vel.setMagnitude(5)
+    
+            this.loc.add(this.vel)
         }
         
 
