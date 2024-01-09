71712637f3af83faaeb883cadc089331e6dc2021
Katelyn Fung <812644@seq.org>
2024-01-09 10:41:17 -0800

    109

--- Food2KGF.js
+++ Food2KGF.js
@@ -1,7 +1,23 @@
 class Food2KGF extends Food {
     constructor(loc, vel, sz, wrld){
         super(loc, vel, sz, wrld);
-        
+        this.arr = [];
+        for(let i = 0; i<100; i++){
+            this.arr.add(new Particle(this.loc.x, this.loc.y, this.sz));
+        }
     }
 
-}
+    update(){
+        this.arr.push(new Particle(this.loc.x, this.loc.y, this.sz));
+
+    }
+
+    
+          
+        
+
+    }
+
+
+
+
